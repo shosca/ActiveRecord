279aaf49caa27065db88aabe1ddb347a426139c5
Serkan Hosca <serkan@hosca.com>
2012-06-25 13:21:14 -0400

    Provide access to persister for model collections

--- src/Castle.ActiveRecord/Model.cs
+++ src/Castle.ActiveRecord/Model.cs
@@ -14,15 +14,29 @@ namespace Castle.ActiveRecord
 	/// </summary>
 	public class Model
 	{
+		public class Collection {
+			public CollectionType Type { get; private set; }
+			public ICollectionPersister Persister { get; private set; }
+
+			public Collection(CollectionType type, ICollectionPersister persister) {
+				if (type == null) throw new ArgumentNullException("type");
+				if (persister == null) throw new ArgumentNullException("persister");
+
+				Type = type;
+				Persister = persister;
+			}
+		}
+
 		public Type Type { get; private set; }
 		public IClassMetadata Metadata { get; private set; }
 		public KeyValuePair<string, IType> PrimaryKey { get; private set; }
 		public IDictionary<string, IType> Properties { get; private set; }
 		public IDictionary<string, ComponentType> Components { get; private set; }
-		public IDictionary<string, EntityType> BelongsTos { get; private set; }
+		public IDictionary<string, ManyToOneType> BelongsTos { get; private set; }
 		public IDictionary<string, OneToOneType> OneToOnes { get; private set; }
-		public IDictionary<string, CollectionType> HasManys { get; private set; }
-		public IDictionary<string, CollectionType> HasAndBelongsToManys { get; private set; }
+		public IDictionary<string, AnyType> Anys { get; private set; }
+		public IDictionary<string, Collection> HasManys { get; private set; }
+		public IDictionary<string, Collection> HasAndBelongsToManys { get; private set; }
 
 		public Model(ISessionFactory sessionfactory, Type type)
 		{
@@ -33,10 +47,11 @@ namespace Castle.ActiveRecord
 			Metadata = sessionfactory.GetClassMetadata(Type);
 			var properties = new Dictionary<string, IType>();
 			var components = new Dictionary<string, ComponentType>();
-			var belongsTos = new Dictionary<string, EntityType>();
+			var belongsTos = new Dictionary<string, ManyToOneType>();
 			var oneToOnes = new Dictionary<string, OneToOneType>();
-			var hasManys = new Dictionary<string, CollectionType>();
-			var hasAndBelongsToManys = new Dictionary<string, CollectionType>();
+			var anys = new Dictionary<string, AnyType>();
+			var hasManys = new Dictionary<string, Collection>();
+			var hasAndBelongsToManys = new Dictionary<string, Collection>();
 
 			PrimaryKey = new KeyValuePair<string, IType>(Metadata.IdentifierPropertyName, Metadata.IdentifierType);
 			foreach (var name in Metadata.PropertyNames)
@@ -52,24 +67,29 @@ namespace Castle.ActiveRecord
 				}
 				else if (prop is ManyToOneType)
 				{
-					belongsTos.Add(name, (EntityType)prop);
+					belongsTos.Add(name, (ManyToOneType)prop);
+				}
+				else if (prop is AnyType)
+				{
+					anys.Add(name, (AnyType)prop);
 				}
 				else if (prop is CollectionType)
 				{
 					var ctype = (CollectionType)prop;
-					var reltype = ctype.ReturnedClass.GetGenericArguments().FirstOrDefault();
+					var persister = sessionfactory.GetCollectionMetadata(ctype.Role) as ICollectionPersister;
+					if (persister == null) return;
+
+					var reltype = persister.ElementType.ReturnedClass;
 					var childmetadata = sessionfactory.GetClassMetadata(reltype);
 					if (childmetadata == null) return;
 
-					var persister = sessionfactory.GetCollectionMetadata(ctype.Role) as ICollectionPersister;
-					if (persister == null) return;
-					if (persister.IsOneToMany)
+					if (persister.IsManyToMany)
 					{
-						hasManys.Add(name, ctype);
+						hasAndBelongsToManys.Add(name, new Collection(ctype, persister));
 					}
-					else if (persister.IsManyToMany)
+					else
 					{
-						hasAndBelongsToManys.Add(name, ctype);
+						hasManys.Add(name, new Collection(ctype, persister));
 					}
 				}
 				else
@@ -79,10 +99,11 @@ namespace Castle.ActiveRecord
 			}
 			Properties = new ReadOnlyDictionary<string, IType>(properties);
 			Components = new ReadOnlyDictionary<string, ComponentType>(components);
-			BelongsTos = new ReadOnlyDictionary<string, EntityType>(belongsTos);
+			BelongsTos = new ReadOnlyDictionary<string, ManyToOneType>(belongsTos);
 			OneToOnes = new ReadOnlyDictionary<string, OneToOneType>(oneToOnes);
-			HasManys = new ReadOnlyDictionary<string, CollectionType>(hasManys);
-			HasAndBelongsToManys = new ReadOnlyDictionary<string, CollectionType>(hasAndBelongsToManys);
+			Anys = new ReadOnlyDictionary<string, AnyType>(anys);
+			HasManys = new ReadOnlyDictionary<string, Collection>(hasManys);
+			HasAndBelongsToManys = new ReadOnlyDictionary<string, Collection>(hasAndBelongsToManys);
 		}
 	}
 }
